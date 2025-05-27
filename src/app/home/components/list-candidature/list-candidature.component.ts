a785625e944d1488cb1bf67f2e89858ab2106b5f
Mbayang <diedhioumbayang1321@gmail.com>
2025-05-27 00:08:12 +0000

    ajout des differents composants

---
+++ src/app/home/components/list-candidature/list-candidature.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-candidature',
+  imports: [],
+  templateUrl: './list-candidature.component.html',
+  styleUrl: './list-candidature.component.css'
+})
+export class ListCandidatureComponent {
+
+}
